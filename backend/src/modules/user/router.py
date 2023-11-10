cec3f099604fad87bb8a6aee77904cc50944971f
JD Solanki <jdsolanki0001@gmail.com>
2023-11-10 08:17:03 +0530

    wip: wip (migrating pc)

--- backend/src/modules/user/router.py
+++ backend/src/modules/user/router.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter, Depends, status
+from datetime import timedelta
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 # DB
 from src.db.deps import get_db
 
-from . import crud, schemas
+from . import auth, crud, deps, schemas
 
 router = APIRouter()
 
@@ -17,6 +20,13 @@ async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 @router.get("/", response_model=list[schemas.UserRead])
 async def read_users(db: Session = Depends(get_db)):
     return crud.get_users(db)
+
+
+@router.get("/me", response_model=schemas.UserRead)
+async def read_user_me(
+    current_user: schemas.UserRead = Depends(deps.get_current_active_user),
+):
+    return current_user
 
 
 @router.get("/{id}", response_model=schemas.UserRead)
@@ -34,3 +44,31 @@ async def update_user(
 @router.delete("/{id}", response_model=None, status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(user_id: int, db: Session = Depends(get_db)):
     crud.delete_user(db, user_id)
+
+
+# Token
+@router.post("/token", response_model=schemas.Token)
+async def get_token(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    user = auth.authenticate_user(
+        db, username=form_data.username, password=form_data.password
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=auth.ACCESS_TOKEN_EXPIRES_MINUTES)
+    access_token = crud.create_access_token(
+        data={"sub": user.username},
+        expires_delta=access_token_expires,
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+    }
